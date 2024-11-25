8321c7d0a96bb1974a4dd089ad1cde229f020225
Billy Kirk <kirk.william.c@gmail.com>
2024-11-25 17:46:17 -0600

    minor updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -203,19 +203,15 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>t", group = "[T]oggle" },
+				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
 			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
 		end,
 	},
 
@@ -798,7 +794,7 @@ require("lazy").setup({
 		"akinsho/toggleterm.nvim",
 		init = function()
 			require("toggleterm").setup({
-				open_mapping = [[<c-`>]],
+				open_mapping = [[<c-\>]],
 				start_in_insert = false,
 				-- direction = "float",
 			})
@@ -818,6 +814,104 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>-", function()
 				require("oil").close()
 			end)
+		end,
+	},
+
+	{
+		-- NOTE: Yes, you can install new plugins here!
+		"mfussenegger/nvim-dap",
+		-- NOTE: And you can specify dependencies as well
+		dependencies = {
+			-- Creates a beautiful debugger UI
+			"rcarriga/nvim-dap-ui",
+
+			-- Required dependency for nvim-dap-ui
+			"nvim-neotest/nvim-nio",
+
+			-- Installs the debug adapters for you
+			"williamboman/mason.nvim",
+			"jay-babu/mason-nvim-dap.nvim",
+
+			-- Add your own debuggers here
+			"leoluz/nvim-dap-go",
+		},
+		keys = function(_, keys)
+			local dap = require("dap")
+			local dapui = require("dapui")
+			return {
+				-- Basic debugging keymaps, feel free to change to your liking!
+				{ "<F5>", dap.continue, desc = "Debug: Start/Continue" },
+				{ "<F1>", dap.step_into, desc = "Debug: Step Into" },
+				{ "<F2>", dap.step_over, desc = "Debug: Step Over" },
+				{ "<F3>", dap.step_out, desc = "Debug: Step Out" },
+				{ "<leader>b", dap.toggle_breakpoint, desc = "Debug: Toggle Breakpoint" },
+				{
+					"<leader>B",
+					function()
+						dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+					end,
+					desc = "Debug: Set Breakpoint",
+				},
+				-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+				{ "<F7>", dapui.toggle, desc = "Debug: See last session result." },
+				unpack(keys),
+			}
+		end,
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			require("mason-nvim-dap").setup({
+				-- Makes a best effort to setup the various debuggers with
+				-- reasonable debug configurations
+				automatic_installation = true,
+
+				-- You can provide additional configuration to the handlers,
+				-- see mason-nvim-dap README for more information
+				handlers = {},
+
+				-- You'll need to check that you have the required things installed
+				-- online, please don't ask me how to install them :)
+				ensure_installed = {
+					-- Update this to ensure that you have the debuggers for the langs you want
+					"delve",
+				},
+			})
+
+			-- Dap UI setup
+			-- For more information, see |:help nvim-dap-ui|
+			dapui.setup({
+				-- Set icons to characters that are more likely to work in every terminal.
+				--    Feel free to remove or use ones that you like more! :)
+				--    Don't feel like these are good choices.
+				icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+				controls = {
+					icons = {
+						pause = "⏸",
+						play = "▶",
+						step_into = "⏎",
+						step_over = "⏭",
+						step_out = "⏮",
+						step_back = "b",
+						run_last = "▶▶",
+						terminate = "⏹",
+						disconnect = "⏏",
+					},
+				},
+			})
+
+			dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+			dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+			dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+			-- Install golang specific config
+			require("dap-go").setup({
+				delve = {
+					-- On Windows delve must be run attached or it crashes.
+					-- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+					detached = vim.fn.has("win32") == 0,
+				},
+			})
 		end,
 	},
 	--
